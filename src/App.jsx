c718c1e87cd6a41d40904364ccc45a032be96992
Linda Vo <volinda2000@gmail.com>
2023-06-07 00:04:01 -0700

    set up files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="bg-black">
+      <ul className="list-none font-playfair">
+        <li>About Me</li>
+        <li>Portfolio</li>
+        <li>Skills</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
