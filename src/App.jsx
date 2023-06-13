d2d74da454c777453178bf1ed43b9c0443a5376b
Linda Vo <volinda2000@gmail.com>
2023-06-12 22:06:25 -0700

    initialize theme colors, add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
+import { useRef, useEffect, useState } from "react";
+
 function App() {
+  const blobRef = useRef(null);
+
+  useEffect(() => {
+    const onMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      const blobX = clientX - blobRef.current.getBoundingClientRect().width / 2;
+      const blobY =
+        clientY - blobRef.current.getBoundingClientRect().height / 2;
+      blobRef.current.style.transform = `translate(${blobX}px, ${blobY}px)`;
+    };
+    if (typeof window != "undefined") {
+      window.addEventListener("mousemove", onMouseMove);
+    }
+  }, []);
+
   return (
-    <div className="bg-black">
-      <ul className="list-none font-playfair">
-        <li>About Me</li>
-        <li>Portfolio</li>
-        <li>Skills</li>
-      </ul>
-    </div>
+    <>
+      <div
+        ref={blobRef}
+        id="cursorBlob"
+        className={`rounded-full bg-offWhite w-[150px] h-[150px] 
+          opacity-40 blur-2xl fixed pointer-events-none z-[999]`}
+      />
+      <div>
+        <ul
+          className="list-none font-playfair text-offWhite text-end text-[4rem] 
+          lowercase italic font-bold mx-5 nav"
+        >
+          <li>
+            <a href="#about" className="nav-item">
+              About Me
+            </a>
+          </li>
+          <li>
+            <a href="#projects" className="nav-item">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="nav-item">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="nav-item">
+              Contact Me
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
-    fontFamily: {
-      playfair: ["Playfair", "serif"],
+    extend: {
+      colors: {
+        pink: "#FF99C0",
+        lightPink: "#FFADCD",
+        yellow: "#FAE373",
+        lightYellow: "#FCEFB0",
+        offWhite: "#F7EDE2",
+        blue: "#475D85",
+        darkBlue: "#3A4B69",
+        darkestBlue: "#161C27",
+      },
+      fontFamily: {
+        playfair: ["Playfair", "serif"],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [],
